014810ba5057a343b77937a1bf6bd45fafbc82c2
Alexei196 <dndy778@gmail.com>
2023-05-04 18:43:58 -0700

    finished montecarlo

--- MergeSort/MergeSort.cpp
+++ MergeSort/MergeSort.cpp
@@ -1,6 +1,49 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<sys/time.h>
+#include<upcxx/upcxx.hpp>
+#include<math.h>
 
 int main(int argc, char** argv) {
-    
+    upcxx::init();
+    int arraySize = 0;
+    if(upcxx::rankme() == 0) {
+        if(argc < 2) {
+            fprintf(stderr, "Not enough arguments!\n");
+            return 1;
+        } 
+        arraySize = atoi(argv[1]);
+        if(arraySize < 1) {
+            fprintf(stderr, "Count cannt be less than 1");
+        }
+    }
+    timeval startTime, endTime;
+    srand(time(NULL) * (1+upcxx::rank_me()));
+    int arraySize = upcxx::broadcast(arraySize, 0).wait();
+    int sizePerProcess = arraySize / upcxx::rank_n();
+
+    int myArray[sizePerProcess];
+    for(int i =0; i < sizePerProcess; ++i) {
+        myArray[sizePerProcess] = rand();
+    }
+
+    for(int level = 0; level <= p; ++level) {
+        for(int idx = 0; idx < 2^p; idx+=2^level) {
+            int dest;
+            if(mod(idx, 2^(level+1)) == 0) {
+                dest = idx;
+            } else {
+                dest = idx - 2^level;
+            }
+
+            //process idx spends array to process dest
+            if(upcxx::rank_me() == idx) {
+                upcxx::rpc(dest, mergeArray, myArray).wait();
+            }
+        }
+    }
+
+
+    upcxx::finalize();
+    return 0;   
 }
--- MonteCarlo/MonteCarlo.cpp
+++ MonteCarlo/MonteCarlo.cpp
@@ -1,58 +1,71 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<sys/time.h>
 #include<upcxx/upcxx.hpp>
 
 #define RAND_D ( -1.0 + ( (double)rand() / (RAND_MAX / 2.0)))
+#define TIME(timeStruct) (double) timeStruct.tv_sec + (double) timeStruct.tv_usec * 0.000001
 
 long long int makeTosses(long long int);
 
+//One argument: toss count in form of a number
 int main(int argc, char** argv) {
-    srand(time(null));
-    if(argc < 2) {
-        fprintf(stderr, "ERROR: Not enough arguments\n");
-        //TODO abort upcxx
-        return 1;
-    }
-    long long int totalTosses = strtoll(argv[1]);
-    if(totalTosses < 1) {
-        fprintf(stderr, "invalid count of tosses\n");
-        //TODO abort upcxx
-        return 1;
-    }
     upcxx::init();
-    int threadCount = upcxx::rank_n(), my_rank = upcxx::rank_me();
-    double wTime = 0;
+    srand(time(NULL) * (1+upcxx::rank_me()));
+    timeval startTime;
+    timeval endTime;
+    long long int totalTosses;
+
+    if(upcxx::rank_me() == 0) {
+        if(argc < 2) {
+            fprintf(stderr, "ERROR: Not enough arguments\n");
+            return 1;
+        }
+        totalTosses = strtoll(argv[1], NULL, 10);
+        if(totalTosses < 1) {
+            fprintf(stderr, "invalid count of tosses\n");
+            return 1;
+        }
+    }
+    gettimeofday(&startTime, NULL);
+    totalTosses = upcxx::broadcast(totalTosses, 0).wait();
 
     //TODO access need to broadcast elements per thread
-    long long int assignedTosses = totalTosses / threadCount;
-    long long int remainderTosses = totalTosses - (assignedTosses * threadCount);
-    //TODO share landedTosses
-    long long int landedTosses = 0;
-    if(my_rank < remainderTosses) {
-        ++assignedTosses;
-    }
-    landedTosses += makeTosses(assignedTosses);
+    long long int assignedTosses = totalTosses / upcxx::rank_n();
+    long long int remainderTosses = totalTosses - (assignedTosses * upcxx::rank_n());
 
-    //TODO Reduction on wTime
-    double wTime = 1;
-    if(my_rank == 0) {
+    long long int processTosses = 0;
+
+ 
+    if(upcxx::rank_me() < remainderTosses) {
+        processTosses = makeTosses(++assignedTosses);
+    } else {
+        processTosses = makeTosses(assignedTosses);
+    }
+    
+    long long int landedTosses = upcxx::reduce_all(processTosses, upcxx::op_fast_add).wait();
+    
+    gettimeofday(&endTime, NULL);
+    double wTime = TIME(endTime) - TIME(startTime);
+    wTime = upcxx::reduce_all(wTime, upcxx::op_fast_max).wait();
+    if(upcxx::rank_me() == 0) {
         double piEstimate = 4.0l * (landedTosses / (double) totalTosses);
-        printf("I estimate pi to be %d\n", piEstimate);
+        printf("I estimate pi to be %f\n", piEstimate);
         printf("My wall time is %f\n", wTime);
     }
+    upcxx::finalize();
     return 0;
 }
 
 long long int makeTosses(long long int assignedTosses) {
     long long int landedSum;
-    for(int toss = 0; toss < assignedTosses; ++i) {
+    for(int toss = 0; toss < assignedTosses; ++toss) {
         double x = RAND_D;
         double y = RAND_D;
         if((x*x + y*y) < 1.0) {
             landedSum+=1ll;
         }
     }
-    //TODO confirm reduction of sums
-    upcxx::finalize();
+    
     return landedSum;
 }
